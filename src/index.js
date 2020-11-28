b930d4a38d9063f4cebbd92171f187eb4e872f6a
yassin19 <yassineldeeb94@gmail.com>
2020-11-28 14:47:03 +0200

    ping timeout

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const app = express()
 const server = http.createServer(app)
 const io = socketio(server, {
   pingInterval: 10000,
-  pingTimeout: 10,
+  pingTimeout: 99999999,
 })
 
 const port = process.env.PORT || 3000
